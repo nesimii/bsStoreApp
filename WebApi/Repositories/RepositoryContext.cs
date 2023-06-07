d79de343b765a348552b415fd51b1e51ab419f5e
Nesimi <hasanesimi01@gmail.com>
2023-06-07 15:17:30 +0300

    connection string

--- WebApi/Repositories/RepositoryContext.cs
+++ WebApi/Repositories/RepositoryContext.cs
@@ -5,6 +5,12 @@ namespace WebApi.Repositories
 {
     public class RepositoryContext : DbContext
     {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
         public DbSet<Book> Books { get; set; }
     }
 }

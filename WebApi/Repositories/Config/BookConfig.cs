85dfb4e6cc7fcd58fb872024f5ed73fb9e0911fb
Nesimi <hasanesimi01@gmail.com>
2023-06-07 15:56:18 +0300

    type configuration

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Karagöz", Price = 75 },
+                new Book { Id = 2, Title = "Mesnevi", Price = 20 },
+                new Book { Id = 3, Title = "Devlet", Price = 100 }
+                );
+        }
+    }
+}

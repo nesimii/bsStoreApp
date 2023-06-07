24a78f78bc2c20fce3da9b9e57f201dab92837c6
Nesimi <hasanesimi01@gmail.com>
2023-06-07 16:06:05 +0300

    IoC

---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+
+        }
+
+    }
+}
